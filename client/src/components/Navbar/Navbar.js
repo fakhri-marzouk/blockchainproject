1931fe4a6486ab9dd447d4b436ed01e7003d6357
fakhri <fakhrimarzouki199@gmail.com>
2022-12-07 22:44:32 +0100

    first

---
+++ client/src/components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+// eslint-disable-next-line
+import React, { useEffect, useState } from "react";
+import "./Navbar.css";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div
+        className="navlogocontainer"
+        onClick={() => {
+          window.location.href = "/";
+        }}
+      >
+        <h1>BankChain</h1>
+      </div>
+      <div className="navlistcontainer">
+        <ul className="navlist">
+          <a href="/">
+            <li className="navlistelements">HOME</li>
+          </a>
+      
+          <a href="/accounts">
+            <li className="navlistelements">ACCOUNTS</li>
+          </a>
+        
+      </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
